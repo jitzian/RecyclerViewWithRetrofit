87e8939902bbed343a214d3a1aa88dd51211c090
jitzian <jitzian@gmail.com>
2016-10-09 16:46:39 -0400

    adding Firebase Push Notifications
    Updates in the Manifest.xml and Firebase Services

--- app/src/androidTest/java/net/sgoliver/android/navigationdrawer/MainActivityTest.java
+++ app/src/androidTest/java/net/sgoliver/android/navigationdrawer/MainActivityTest.java
@@ -24,7 +24,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
  * @author Libin
  */
 @RunWith(AndroidJUnit4.class)
-public class CheeseListActivityTest {
+public class MainActivityTest {
 
     @Rule
 //    public ActivityTestRule<CheeseListActivity> mActivityTestRule = new ActivityTestRule<>(CheeseListActivity.class);
--- app/src/main/java/net/sgoliver/android/navigationdrawer/fragments/Fragment3.java
+++ app/src/main/java/net/sgoliver/android/navigationdrawer/fragments/Fragment3.java
@@ -2,13 +2,22 @@ package net.sgoliver.android.navigationdrawer.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import net.sgoliver.android.navigationdrawer.R;
 
 public class Fragment3 extends Fragment {
+
+    public static final String TAG = Fragment3.class.getName();
+    View rootView;
 
     public Fragment3() {
         // Required empty public constructor
@@ -18,6 +27,59 @@ public class Fragment3 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment3, container, false);
+        rootView = inflater.inflate(R.layout.fragment_fragment3, container, false);
+
+        // If a notification message is tapped, any data accompanying the notification
+        // message is available in the intent extras. In this sample the launcher
+        // intent is fired when the notification is tapped, so any accompanying data would
+        // be handled here. If you want a different intent fired, set the click_action
+        // field of the notification message to the desired intent. The launcher intent
+        // is used when no click_action is specified.
+        //
+        // Handle possible data accompanying notification message.
+        // [START handle_data_extras]
+        if (getActivity().getIntent().getExtras() != null) {
+            for (String key : getActivity().getIntent().getExtras().keySet()) {
+                Object value = getActivity().getIntent().getExtras().get(key);
+                Log.d(TAG, "Key: " + key + " Value: " + value);
+            }
+        }
+        // [END handle_data_extras]
+
+        Button subscribeButton = (Button) rootView.findViewById(R.id.subscribeButton);
+        subscribeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // [START subscribe_topics]
+                Log.d(TAG, "subscribeButton");
+                FirebaseMessaging.getInstance().subscribeToTopic("news");
+                // [END subscribe_topics]
+
+                // Log and toast
+                String msg = getString(R.string.msg_subscribed);
+                Log.d(TAG, msg);
+                Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        Button logTokenButton = (Button) rootView.findViewById(R.id.logTokenButton);
+        logTokenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "logTokenButton");
+                // Get token
+                String token = FirebaseInstanceId.getInstance().getToken();
+
+                // Log and toast
+                String msg = getString(R.string.msg_token_fmt, token);
+                Log.d(TAG, msg);
+                Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+        return rootView;
     }
 }
